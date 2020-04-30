a0451b12c7597fc60a1b08cd500ac431b7d292dc
Tóth András <toth83andras@yahoo.com>
2020-04-30 15:23:23 +0200

    get score from global state

--- src/components/quiz-page/question-footer.tsx
+++ src/components/quiz-page/question-footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './question-footer.scss';
+import { GlobalState } from '../../redux/global-state';
+import { useSelector } from 'react-redux';
 
 export function QuestionFooter() {
+  const scoreFromState = useSelector<GlobalState, number>(({ score }) => score);
+
   return (
     <div className="question-footer">
       <div>
         <label>Score:</label>
-        <span>5</span>
+        <span>{scoreFromState}</span>
       </div>
       <div>
         <button className="btn btn-primary">Next question</button>
