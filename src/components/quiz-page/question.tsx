69c3c1777d8773dfe0cfa1e5029bbee97fbe50dd
Tóth András <toth83andras@yahoo.com>
2020-04-30 15:46:45 +0200

    load actual question from store in quiz

--- src/components/quiz-page/question.tsx
+++ src/components/quiz-page/question.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import './question.scss';
 import { Answer } from './answer';
-import { AnswerIndex } from '../../shared-models/types';
+import { AnswerIndex, QuestionProps } from '../../shared-models/types';
 import { QuestionFooter } from './question-footer';
+import { GlobalState } from '../../redux/global-state';
+import { useSelector } from 'react-redux';
+
+function getCurrentQuestion({ questions, activeQuestionIndex }: GlobalState): QuestionProps {
+  return questions[activeQuestionIndex];
+}
 
 export function Question() {
+  const { question, answers } = useSelector<GlobalState, QuestionProps>(getCurrentQuestion);
+
   return (
     <div className="question">
-      <div className="question-header"><h3>What is the capital of Moroshgovany?</h3></div>
+      <div className="question-header">
+        <h3>{question}</h3>
+      </div>
       <div className="answers">
-        <Answer index={AnswerIndex.A} text="Qualny" isRight />
-        <Answer index={AnswerIndex.B} text="Lakshmir" />
-        <Answer index={AnswerIndex.C} text="Birya" />
-        <Answer index={AnswerIndex.D} text="Lequir" />
+        {Object.keys(answers)
+          .sort()
+          .map((key) => {
+            const answerIndex = key as AnswerIndex;
+            const { text, isRight } = answers[answerIndex];
+
+            return <Answer index={answerIndex} text={text} isRight={isRight} />;
+          })}
       </div>
       <QuestionFooter />
     </div>
