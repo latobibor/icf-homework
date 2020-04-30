b83410655b5f3595d87b3f1589c5c3614fb83756
Tóth András <toth83andras@yahoo.com>
2020-04-30 16:50:57 +0200

    separate default questions from initial state

--- src/redux/global-state.ts
+++ src/redux/global-state.ts
@@ -1,43 +1,5 @@
 import { QuestionProps, AnswerIndex } from '../shared-models/types';
-
-const dummyQuestions: QuestionProps[] = [
-  {
-    question: 'What is the capital of Ghana?',
-    answers: {
-      [AnswerIndex.D]: {
-        text: 'Accra',
-        isRight: true,
-      },
-      [AnswerIndex.C]: { text: 'Mogadishu' },
-      [AnswerIndex.B]: { text: 'Lomé' },
-      [AnswerIndex.A]: { text: 'Luanda' },
-    },
-  },
-  {
-    question: 'How long does it take to cook pasta?',
-    answers: {
-      [AnswerIndex.B]: {
-        text: 'Until it still has some consistency',
-        isRight: true,
-      },
-      [AnswerIndex.C]: { text: '5 minutes' },
-      [AnswerIndex.D]: { text: '10 minutes' },
-      [AnswerIndex.A]: { text: '15 minutes' },
-    },
-  },
-  {
-    question: 'Wenn ist das Nunnstück git und Slotermeyer?',
-    answers: {
-      [AnswerIndex.A]: {
-        text: 'Ja! Beiherhund das oder die Flipperwaldt gersput.',
-        isRight: true,
-      },
-      [AnswerIndex.B]: { text: 'Nein' },
-      [AnswerIndex.C]: { text: 'Was ist ein gersput?' },
-      [AnswerIndex.D]: { text: 'Monty Python' },
-    },
-  },
-];
+import { defaultQuestions } from './default-questions';
 
 export type GlobalState = {
   activeQuestionIndex: number;
@@ -50,7 +12,7 @@ export type GlobalState = {
 
 export const initialState: GlobalState = {
   activeQuestionIndex: 0,
-  questions: dummyQuestions,
+  questions: defaultQuestions,
   score: 0,
   gameOver: false,
   isCurrentQuestionAnswered: false,
