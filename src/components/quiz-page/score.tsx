f14d3370c90b0c6fa33c3a981970e87486968745
Tóth András <toth83andras@yahoo.com>
2020-04-30 16:54:30 +0200

    style score with button

--- src/components/quiz-page/score.tsx
+++ src/components/quiz-page/score.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { GlobalState } from '../../redux/global-state';
+import './score.scss';
 
 export function Score() {
   const score = useSelector<GlobalState, number>(({ score }) => score);
   const numberOfQuestions = useSelector<GlobalState, number>(({ questions }) => questions.length);
 
   return (
-    <span>
+    <button className="score btn btn-light" disabled>
       {score} / {numberOfQuestions}
-    </span>
+    </button>
   );
 }
