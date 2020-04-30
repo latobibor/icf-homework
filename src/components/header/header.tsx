83ef2ec2df7ddb0a128939a28543ba255d4a0227
Tóth András <toth83andras@yahoo.com>
2020-04-30 14:38:13 +0200

    disable menu item if there are no questions

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
+import { GlobalState } from '../../redux/global-state';
+import { useSelector } from 'react-redux';
+
+// maybe it's not the best name for clean coding but I love it :)
+function isThereAnyQuestion(state: GlobalState): boolean {
+  return state.questions.length > 0;
+}
 
 export function Header() {
+  const shouldNewGameBeActive = useSelector<GlobalState, boolean>(isThereAnyQuestion);
+
   return (
     <header>
       <div className="header-menu disclaimer">by András Dániel Tóth</div>
 
-      <Link to="/game">
+      <Link to="/game" className={`${shouldNewGameBeActive ? '' : 'disabled'}`}>
         <div className="header-menu menu-item">New game!</div>
       </Link>
 
